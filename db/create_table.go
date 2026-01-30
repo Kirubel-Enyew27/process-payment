b9b84ab8d02e1e7f5a44c9e95c8d08c56934129c
Kirubel Enyew <enyewkirubel76@gmail.com>
2026-01-30 14:39:07 +0300

    update table schema

--- db/create_table.go
+++ db/create_table.go
@@ -6,21 +6,40 @@ import (
 )
 
 func CreateTable(db *sql.DB) error {
-	createTableSql := `
+	createUserTableSql := `
+	 CREATE TABLE IF NOT EXISTS users(
+	 	id SERIAL PRIMARY KEY,
+		phone VARCHAR(100),
+		role VARCHAR(100),
+		status VARCHAR(100),
+		created_at DATE
+	 )
+	`
+	_, err := db.Exec(createUserTableSql)
+	if err != nil {
+		return fmt.Errorf("Failed to create table: %v", err)
+	}
+
+	createTransactionTableSql := `
 	 CREATE TABLE IF NOT EXISTS transactions(
 	 	id SERIAL PRIMARY KEY,
+		user_id INT, 
 		amount NUMERIC,
 		phone VARCHAR(100),
 		reason VARCHAR(100),
 		reference VARCHAR(100),
 		status VARCHAR(100),
-		created_at DATE
+		created_at DATE, 
+    CONSTRAINT fk_user
+      FOREIGN KEY(user_id)
+        REFERENCES users(id)
 	 )
 	`
-	_, err := db.Exec(createTableSql)
+	_, err = db.Exec(createTransactionTableSql)
 	if err != nil {
 		return fmt.Errorf("Failed to create table: %v", err)
 	}
-	fmt.Println("Table created successfully.")
+
+	fmt.Println("Tables created successfully.")
 	return nil
 }
